cfc006430dd2aa734506ee08f819600946463816
aebibtech <paul.camano@gmail.com>
2026-06-05 03:53:01 +0800

    fix: ephemeral opentofu storage

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,6 +1,11 @@
 terraform {
   required_version = ">= 1.6.0"
 
+  backend "gcs" {
+    bucket = "teamchords-tfstate"
+    prefix = "terraform/state"
+  }
+
   required_providers {
     google = {
       source  = "hashicorp/google"
